11b8b3f0137c479bb9569e4dfb3dc720da2109df
BerGP <bernardogameplaysofc4596@gmail.com>
2026-07-24 20:51:49 -0300

    Shader da High

---
+++ scripts/songs/high-reinvented.hx
@@ -0,0 +1,26 @@
+import flixel.addons.display.FlxRuntimeShader;
+import openfl.filters.ShaderFilter;
+import funkin.Paths;
+import funkin.play.PlayState;
+import funkin.play.song.Song;
+
+class HighReinventedSunset extends Song
+{
+	var shader:FlxRuntimeShader;
+	var shaderFilter:ShaderFilter;
+
+	function new() { super('high-reinvented'); }
+
+	override function onCreate()
+	{
+		final shaderCode:String = Assets.getText( Paths.frag('sunset') );
+		shader = new FlxRuntimeShader(shaderCode);
+		shaderFilter = new ShaderFilter(shader);
+
+		FlxG.camera.filters ??= [];
+		FlxG.camera.filters.push(shaderFilter);
+	}
+
+	override function onGameOver() { FlxG.camera.filters.remove(shaderFilter); }
+	override function onSongRetry() { FlxG.camera.filters.push(shaderFilter); }
+}
